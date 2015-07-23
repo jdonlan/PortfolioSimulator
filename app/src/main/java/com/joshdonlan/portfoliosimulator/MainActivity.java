79a718bbde19f50c1341da6e2e04ed2ef2b46645
Joshua Donlan <jdonlan@fullsail.com>
2015-07-23 14:28:55 -0400

    Added Material toolbar via v7 support with search.

--- app/src/main/java/com/joshdonlan/portfoliosimulator/MainActivity.java
+++ app/src/main/java/com/joshdonlan/portfoliosimulator/MainActivity.java
@@ -1,23 +1,42 @@
 package com.joshdonlan.portfoliosimulator;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.joshdonlan.portfoliosimulator.objetcs.Stock;
 import com.joshdonlan.portfoliosimulator.objetcs.StockTask;
 import com.joshdonlan.utils.AndroidConnectivity;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity implements StockTask.StockListener{
 
     public static final String TAG = "MainActivity";
+
+    private Toolbar toolbar;
+    private boolean mSearchOpened = false;
+    private MenuItem mSearch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         if(AndroidConnectivity.getDataStatus(this) >= AndroidConnectivity.ONLINE) {
 
@@ -39,11 +58,103 @@ public class MainActivity extends Activity {
 
     }
 
+    @Override
+    public void onBackPressed() {
+        if(mSearchOpened) {
+            toggleMenuSearch();
+            return;
+        }
+        super.onBackPressed();
+    }
+
+    //MENU
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (id){
+            case R.id.action_settings:
+                break;
+            case R.id.action_search:
+                mSearch = item;
+                toggleMenuSearch();
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 
 
+    private void toggleMenuSearch(){
+
+        ActionBar action = getSupportActionBar();
+
+        if(mSearchOpened){
+
+            action.setDisplayShowCustomEnabled(false);
+            action.setDisplayShowTitleEnabled(true);
+
+            EditText searchField = (EditText)action.getCustomView().findViewById(R.id.et_search);
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(searchField.getWindowToken(), 0);
+
+            mSearch.setIcon(R.drawable.ic_search);
+            mSearchOpened = false;
+
+        } else {
+
+            action.setDisplayShowCustomEnabled(true);
+            action.setCustomView(R.layout.toolbar_search);
+            action.setDisplayShowTitleEnabled(false);
+
+            EditText searchField = (EditText)action.getCustomView().findViewById(R.id.et_search);
+
+            searchField.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+                @Override
+                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                        toggleMenuSearch();
+                        doSearch(v.getText().toString());
+                        return true;
+                    }
+                    return false;
+                }
+            });
+
+            searchField.requestFocus();
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.showSoftInput(searchField, InputMethodManager.SHOW_IMPLICIT);
+
+            mSearch.setIcon(R.drawable.ic_close);
+            mSearchOpened = true;
+        }
+    }
+
+    private void doSearch(String _query){
+        StockTask stockTask = new StockTask(this,true);
+        stockTask.execute(_query);
+    }
 
 
-
-
-
+    @Override
+    public void receiveStockData(Stock[] _stocks) {
+        Stock stock = _stocks[0];
+        if(stock != null) {
+            Intent loadDetails = new Intent(this, StockDetailActivity.class);
+            Bundle extras = new Bundle();
+            extras.putSerializable(StockDetailActivity.STOCK, stock);
+            loadDetails.putExtras(extras);
+            startActivity(loadDetails);
+        } else {
+            Toast.makeText(this, getResources().getString(R.string.toast_symbol_not_found),Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
 }
--- app/src/main/java/com/joshdonlan/portfoliosimulator/objetcs/StockTask.java
+++ app/src/main/java/com/joshdonlan/portfoliosimulator/objetcs/StockTask.java
@@ -23,20 +23,27 @@ public class StockTask extends AsyncTask<String,Void,Stock[]> {
 
     public static final String TAG = "StockTask";
 
-    public Context mContext;
+    private Context mContext;
+    private boolean mActiveSearch;
 
     public interface StockListener{
         public void receiveStockData(Stock[] _stocks);
     }
 
     public StockTask(Context _context){
+        this(_context, false);
+    }
+
+    public StockTask(Context _context, boolean _activeSearch) {
         mContext = _context;
+        mActiveSearch = _activeSearch;
     }
 
     @Override
     protected Stock[] doInBackground(String... params) {
         Stock[] stocks = new Stock[params.length];
 
+        int counter = 0;
         for(String symbol : params) {
             JSONObject jsonStock = null;
             try {
@@ -44,12 +51,15 @@ public class StockTask extends AsyncTask<String,Void,Stock[]> {
                 String yql = "select * from csv where url='http://download.finance.yahoo.com/d/quotes.csv?s=" + symbol + "&f=sl1d1t1c1ohgvp2&e=.csv' and columns='symbol,price,date,time,change,open,high,low,volume,chgpct'";
                 String qs = URLEncoder.encode(yql, "UTF-8");
                 URL queryURL = new URL(baseURL + "?q=" + qs + "&format=json");
+                Log.d(TAG,"Query : " + queryURL.toString());
                 HttpURLConnection conn = (HttpURLConnection) queryURL.openConnection();
                 String result = IOUtils.toString(conn.getInputStream());
                 JSONObject jsonResult = new JSONObject(result);
                 jsonStock = (jsonResult != null) ? jsonResult.getJSONObject("query").getJSONObject("results").getJSONObject("row") : null;
-                Stock stock = (jsonStock != null) ? new Stock(jsonStock) : null;
-                boolean isSaved = AndroidIO.writeInternal(mContext,symbol+".dat",stock);
+                Stock stock = (jsonStock != null && jsonStock.getString("price").compareTo("N/A")!=0) ? new Stock(jsonStock) : null;
+                if(stock != null) AndroidIO.writeInternal(mContext,symbol+".dat",stock);
+                stocks[counter] = stock;
+                counter++;
             } catch (UnsupportedEncodingException e) {
                 Log.e(TAG, "Error encoding API request.");
             } catch (IOException e) {
@@ -66,7 +76,7 @@ public class StockTask extends AsyncTask<String,Void,Stock[]> {
     @Override
     protected void onPostExecute(Stock[] _stocks) {
         super.onPostExecute(_stocks);
-        if(mContext instanceof StockListener){
+        if(mContext instanceof StockListener && mActiveSearch){
             ((StockListener) mContext).receiveStockData(_stocks);
         }
     }
